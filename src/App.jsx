b6883dfd7d18dacebb8500677d3190e13b896660
Daniel <danielrojo@gmail.com>
2022-05-11 17:36:00 +0200

    calculator first version

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './App.css';
+import Calculator from "./components/calculator/Calculator";
+class App extends Component {
+
+    state = {  }
+    render() { 
+        return ( <Calculator></Calculator> );
+    }
+}
+ 
+export default App;
