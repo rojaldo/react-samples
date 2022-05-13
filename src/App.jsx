85aa291b8c440d42d04a00580c592f2de05ba957
Daniel <danielrojo@gmail.com>
2022-05-13 17:44:58 +0200

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { Component } from "react";
 import './App.css';
 import Calculator from "./components/calculator/Calculator";
+import { Tabs } from "react-bootstrap";
+import { Tab } from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 class App extends Component {
 
-    state = {  }
-    render() { 
-        return ( <Calculator></Calculator> );
+    state = {}
+    render() {
+        return (
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                <Tab eventKey="home" title="Calculator">
+                    <Calculator></Calculator>
+                </Tab>
+                <Tab eventKey="profile" title="Heroes">
+                    Heroes
+                </Tab>
+            </Tabs>
+        );
     }
 }
- 
+
 export default App;
