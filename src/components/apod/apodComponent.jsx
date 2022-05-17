1488543939d917592552ae92f8a54014fafece4d
Daniel <danielrojo@gmail.com>
2022-05-17 17:28:38 +0200

    apod component modified

--- src/components/apod/apodComponent.jsx
+++ src/components/apod/apodComponent.jsx
@@ -37,23 +37,23 @@ class ApodComponent extends Component {
         this.getApod();
     }
     render() {
-        let media = <img class="m-auto" src={this.state.apod.url} alt={this.state.apod.title} />;
+        let media = <img className="m-auto" src={this.state.apod.url} alt={this.state.apod.title} />;
         if(this.state.apod.media_type === 'video') {
-            media = <iframe width="560" className="mx-auto" height="315" src={this.state.apod.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            media = <iframe width="560" className="mx-auto" height="315" src={this.state.apod.url} title={this.state.apod.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         }
         return (
-            <div class="container">
+            <div className="container">
                 <DatePicker selected={this.state.selectedDate} onChange={(date) => this.updateDate(date)} />
-                <div class="jumbotron">
-                    <h1 class="display-4">{this.state.apod.title}</h1>
-                    <p class="lead">{this.state.apod.date}</p>
-                    <div class="d-flex">
+                <div className="jumbotron">
+                    <h1 className="display-4">{this.state.apod.title}</h1>
+                    <p className="lead">{this.state.apod.date}</p>
+                    <div className="d-flex">
                     {media}    
                     </div>
-                    <hr class="my-2"/>
+                    <hr className="my-2"/>
                         <p>{this.state.apod.explanation}</p>
-                        <p class="lead">
-                            <a class="btn btn-primary btn-lg" href={this.state.apod.hdurl} role="button">HD Image</a>
+                        <p className="lead">
+                            <a className="btn btn-primary btn-lg" href={this.state.apod.hdurl} role="button">HD Image</a>
                         </p>
                 </div>
             </div>
